c1704553cf35f2d3c982c63a5f32d6f2aafcef96
Philip Sopher <phsophe@gmail.com>
2015-01-25 13:50:03 -0500

    Exercise 16~~Lots of Questions About It

--- SwiftExercises.playground/section-1.swift
+++ SwiftExercises.playground/section-1.swift
@@ -7,12 +7,14 @@ Strings
 */
 
 func favoriteCheeseStringWithCheese(cheese: String) -> String {
-    // WORK HERE
+    var cheese = "cheddar"
     return cheese
 }
 
-let fullSentence = favoriteCheeseStringWithCheese("cheddar")
+var fullSentence = favoriteCheeseStringWithCheese("cheddar")
 // Make fullSentence say "My favorite cheese is cheddar."
+
+fullSentence = "My favorite cheese is \(favoriteCheeseStringWithCheese)"
 
 /*
 
@@ -20,13 +22,14 @@ Arrays & Dictionaries
 
 */
 
-let numberArray = [1, 2, 3, 4]
+var numberArray = [1, 2, 3, 4]
 // Add 5 to this array
-// WORK HERE
+numberArray += [5]
 
-let numberDictionary = [1 : "one", 2 : "two", 3 : "three", 4 : "four"]
+var numberDictionary = [1 : "one", 2 : "two", 3 : "three", 4 : "four"]
 // Add 5 : "five" to this dictionary
-// WORK HERE
+numberDictionary[5] = "five"
+numberDictionary
 
 /*
 
@@ -35,10 +38,15 @@ Loops
 */
 
 // Use a closed range loop to print 1 - 10, inclusively
-// WORK HERE
+for var i = 1; i <= 10; i++ {
+    print ("\(i) ")
+}
+
 
 // Use a half-closed range loop to print 1 - 10, inclusively
-// WORK HERE
+for var q = 1; q < 11; q++ {
+    print ("\(q) ")
+}
 
 let worf = [
     "name": "Worf",
@@ -55,10 +63,18 @@ let picard = [
 
 let characters = [worf, picard]
 
+var returnValue : Array
+
 func favoriteDrinksArrayForCharacters(characters:Array<Dictionary<String, String>>) -> Array<String> {
     // return an array of favorite drinks, like ["prune juice", "tea, Earl Grey, hot"]
-    // WORK HERE
-    return []
+    for var x = 1; x <= 2; x++ {
+        for (characteristic, descriptionOfCharacteristic) in characters[x] {
+            if characteristic = "favorite drink" {
+                returnValue += [\descriptionOfCharacteristic, ]
+            }
+        }
+    }
+    return [returnValue]
 }
 
 let favoriteDrinks = favoriteDrinksArrayForCharacters(characters)
@@ -75,7 +91,12 @@ Functions
 
 let strings = ["milk", "eggs", "bread", "challah"]
 
-// WORK HERE - make your function and pass `strings` in
+func stringSeparatedBySemiColon(items : String) {
+    for var i = 1; i <= 4; i++ {
+        items = "\strings[i];"
+    }
+    
+}
 
 let expectedOutput = "milk;eggs;bread;challah"
 
@@ -88,4 +109,6 @@ Closures
 let cerealArray = ["Golden Grahams", "Cheerios", "Trix", "Cap'n Crunch OOPS! All Berries", "Cookie Crisp"]
 
 // Use a closure to sort this array alphabetically
-// WORK HERE
+let cerealsSortedAlphabetically = sorted(cerealArray, >)
+
+
